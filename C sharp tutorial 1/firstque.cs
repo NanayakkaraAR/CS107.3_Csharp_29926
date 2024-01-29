bda6d1d0393302f31c70730336b96b23c44fa30a
ashini reshana nanayakkara <132426734+NanayakkaraAR@users.noreply.github.com>
2024-01-29 23:23:46 +0530

    Add files via upload

---
+++ C sharp tutorial 1/firstque.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tutorial_01
+{
+    internal class firstque
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the length");
+            int l= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the width");
+            int w= Convert.ToInt32(Console.ReadLine());
+
+            CalArea(l,w);
+
+        }
+
+        static void CalArea(int length, int width)
+        {
+            int area; 
+                area=length * width ;
+            Console.WriteLine("The area is "  + area);
+            Console.ReadLine();
+        }
+           
+
+            
+
+    }
+}
