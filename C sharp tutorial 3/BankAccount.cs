8e1c57624fb7e0d05d2575020aea47ced7a1184b
ashini reshana nanayakkara <132426734+NanayakkaraAR@users.noreply.github.com>
2024-02-13 17:00:04 +0530

    Add files via upload

---
+++ C sharp tutorial 3/BankAccount.cs
@@ -0,0 +1,34 @@
+﻿namespace tutorial_2;
+
+class BankAccount
+{
+    private int AccountNumber;
+    public double Balance=1000;
+    /*public BankAccount(double initialBal)
+    {
+        Balance = initialBal;
+    }*/
+    public void deposit(double amount)
+    {
+        Balance += amount;
+    }
+
+    static void Main(string[] args)
+    {
+        BankAccount Acc1 = new BankAccount();
+        Console.WriteLine("Enter account number: ");
+        Acc1.AccountNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Initial Balance is: " + Acc1.Balance);
+    
+        Console.WriteLine("Enter amount to deposit: ");
+        double inputAmount = Convert.ToDouble(Console.ReadLine());
+    
+        Acc1.deposit(inputAmount);
+        Console.WriteLine("New Balance is: " + Acc1.Balance);
+    
+        Console.ReadLine();
+    }
+
+}
+
---
+++ C sharp tutorial 3/TemperatureTracker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace tutorial_2
+{
+	internal class TemperatureTracker 
+	{
+		static void Main(string[] args)
+		{
+			TemperatureTracker tracker = new TemperatureTracker();
+
+			tracker.DailyTemperature();
+			Console.WriteLine("\n");
+			tracker.Report();
+
+			Console.ReadLine();
+		}
+		
+	}
+}
+
