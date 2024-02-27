c4a222b149845b18b4bfbffcfe87d79310bf55ad
ashini reshana nanayakkara <132426734+NanayakkaraAR@users.noreply.github.com>
2024-02-28 00:03:44 +0530

    Add files via upload

---
+++ labsheet_3/lab_sheet3/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+namespace que_1;
+
+public class Course
+{
+    private string courseName;
+    private string instructorName;
+    private double grade;
+
+    //public properties
+    public string CourseName
+    {
+        get { return courseName; }
+    }
+
+    public double Grade
+    {
+        get { return grade; }
+    }
+
+    //constructor
+    public Course(string courseName, string instructorName,double grade)
+    {
+        this.courseName = courseName;
+        SetInstructorName(instructorName);
+        SetGrade(grade);
+    }
+
+    //public method to set instructor's name with validation
+    public void SetInstructorName(string instructorName)
+    {
+        if (string.IsNullOrWhiteSpace(instructorName))
+        {
+            throw new ArgumentException("Instructor cannot be empty or null");
+        }
+        this.instructorName = instructorName;
+    }
+
+    //private method to calculate letter grade
+    private string CalculateLetterGrade()
+    {
+        if (grade > 90)
+            return "A";
+        else if (grade >= 80)
+            return "B";
+        else if (grade >= 70)
+            return "C";
+        else if (grade >= 60)
+            return "D";
+        else
+            return "F";
+    }
+
+    //public method to print course information
+    public void PrintCourseInfo()
+    {
+        string letterGrade = CalculateLetterGrade();
+        Console.WriteLine($"Course Name:{courseName}");
+        Console.WriteLine($"Instructor Name:{instructorName}");
+        Console.WriteLine($"Letter Grade:{letterGrade}");
+        Console.ReadLine();
+    }
+
+    //private method to set grade validation
+    private void SetGrade(double grade)
+    {
+        if (grade< 0 || grade>100)
+        {
+            throw new ArgumentException("Grade must be between 0 and 100");
+        }
+        this.grade = grade;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            //create a new course object
+            Course course = new Course("Computer Science", "Dr.Smith", 85);
+
+            //print course infortmation
+            course.PrintCourseInfo();
+
+            //try setting invalid instructor name
+            //this should throw an exception
+            //course.SetInstructorName("");//uncomment this line to see the exception
+            //try setting invalid grade
+            //this should throw an exception
+            //course.SetGrade(105);//uncomment this line to see the exception
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"Error:{ex.Message}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ labsheet_3/que_2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace que_2;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Employee employee = new Employee(101, "JOhn Doe, 50000.00");
+
+        Console.WriteLine("Employee Information");
+        employee.DisplayEmployeeInfo();
+
+        Console.WriteLine($"Employee ID{employee.EmployeeID}");
+
+
+        employee.FullName = "Jane Smith";
+        Console.WriteLine($"Updateed full name {employee.FullName}");
+        employee.Salary = 55000;
+        Console.WriteLine($"updated salary{employee.salary}");
+
+        Console.ReadLine();
+
+    }
+}
+
+internal class Employee
+{
+    public int employeeID;
+    public string fullName;
+    public double salary;
+    public Employee(int employeeID, string FullName, double salary)
+
+    {
+        this.employeeID = employeeID;
+        this.fullName = FullName;
+        this.salary = salary;
+    }
+
+    public int EmployeedID => employeeID;
+    public string FullName
+
+    {
+        get => FullName;
+        set => FullName = value;
+    }
+
+public double Salary
+
+    {
+        get => salary;
+        set => salary = value;
+    }
+
+public void DisplayEmployeeInfo()
+
+    {
+        Console.WriteLine($"Employee ID {employeeID}");
+        Console.WriteLine($"Full Name {employeeID}");
+        Console.WriteLine($"Salary {employeeID}");
+    }   
+}
+
